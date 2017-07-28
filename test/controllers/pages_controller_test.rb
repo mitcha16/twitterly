7c0c662870137a36c77532c42343174eac511434
mitchashby16 <mitchashby16@gmail.com>
2017-07-28 19:58:30 -0400

    named project

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,19 +4,19 @@ class PagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | TwitterClone"
+    assert_select "title", "Home | Twitterly"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | TwitterClone"
+    assert_select "title", "Help | Twitterly"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | TwitterClone"
+    assert_select "title", "About | Twitterly"
   end
 
 end
