a5566539478f012085274e1df974334790f2609d
mitchashby16 <mitchashby16@gmail.com>
2017-07-29 16:43:50 -0400

    user can log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  
   get 'users/new'
-
   get 'pages/home'
-
   get 'pages/about'
 
   root 'pages#home'
-
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   resources :users
